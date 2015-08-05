a2f8c004eeb7e0776a0936c5a05cd3c07ca5b2f6
neilellis <hello@neilellis.me>
2015-08-05 16:35:30 +0100

    Clean

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM sillelien/base-java
-COPY dollar/dist/dollar /app
-VOLUME /build
-WORKDIR /build
-ENTRYPOINT ["/app/bin/dollar"]
--- pack/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eu
-cd $(dirname $0)/..
-
-#mv ../dist/dollar.tgz ../dist/dollar-${1}-${2}.tgz
-aws s3 cp dollar/dist/dollar.tgz s3://sillelien-maven-repo/dist/dollar/dollar-${release:-snapshot}.tgz
