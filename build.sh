5c24f151ccfa38c57ab8dfbee4f739b4ead32bda
neilellis <hello@neilellis.me>
2015-08-05 15:24:03 +0100

    Initial work

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ set -u
 rm -rf ~/.dollar/typelearning.*
 mvn -e -T 1C -Dmaven.test.skip=true -Drat.skip=true  -Dmaven.javadoc.skip=true -DgenerateReports=false install
 #TODO remove the || : bodge operator
-mvn -e -Dorg.xml.sax.driver="com.sun.org.apache.xerces.internal.parsers.SAXParser" site:site site:stage || :
+mvn -e -Dorg.xml.sax.driver=com.sun.org.apache.xerces.internal.parsers.SAXParser site:site site:stage || :
 cd -
 ./build-docs.sh
 ./pack/pack.sh
