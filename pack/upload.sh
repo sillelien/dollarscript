16e9c451813f15cc30308576fbd003ca1bb7fad3
neilellis <hello@neilellis.me>
2015-08-05 16:06:22 +0100

    Initial work

--- pack/upload.sh
+++ pack/upload.sh
@@ -2,4 +2,4 @@
 cd $(dirname $0)/..
 
 #mv ../dist/dollar.tgz ../dist/dollar-${1}-${2}.tgz
-aws s3 cp dollar/dist/dollar.tgz s3://sillelien-maven-repo/dist/dollar/dollar-${release:-snapshot}.tgz
+aws s3 cp dollar/dist/dollar.tgz s3://sillelien-maven-repo/dist/dollarscript/dollarscript-${RELEASE:-snapshot}.tgz
